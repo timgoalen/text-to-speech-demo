792fe62d982279b4519908aa6032dcf73db719fd
Tim Goalen <timgoalen@hotmail.com>
2024-05-29 13:09:21 +0000

    feat: implement browser speech synthesis api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-console.log("Script is here")
+function initSpeechSynthesis() {
+  // -- DOM ELEMENTS --
+  
+  const synth = window.speechSynthesis;
+  const speakBtn = document.getElementById("speak-btn");
+  const clearBtn = document.getElementById("clear-btn");
+  const textArea = document.getElementById("text-area");
+
+  // -- FUNCTIONS --
+
+  function speakText() {
+    const text = textArea.value;
+    const utterance = new SpeechSynthesisUtterance(text);
+    // Return if synth is already speaking, or the text area is empty.
+    if (synth.speaking || !textArea.value) {
+      return;
+    }
+    synth.speak(utterance);
+  }
+
+  function clearTextArea() {
+    textArea.value = "";
+    textArea.focus();
+  }
+
+  // -- EVENT LISTENERS --
+
+  speakBtn.addEventListener("click", speakText);
+  clearBtn.addEventListener("click", clearTextArea);
+}
+
+document.addEventListener("DOMContentLoaded", initSpeechSynthesis);
